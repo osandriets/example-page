47bd560936444f1d5cc0f33143a4c851e1d61583
osadnriets@gmail.com <gjx2890-12345>
2018-05-02 23:32:32 +0300

    first

---
+++ README.md
@@ -0,0 +1,26 @@
+# webpack project 
+
+ 
+## Environment setup 
+
+```sh
+  $ npm install
+```
+
+## Development
+
+Start the Webpack server (includes live reloading when you change files):
+
+```sh
+  $ npm start
+```
+
+Open [http://localhost:3001](http://localhost:3001) in a browser. `./src/main.js` is the entry point.
+
+
+## Bundling 
+
+```sh
+  $ npm run bundle
+```
+
---
+++ src/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+require('./main.scss');
+// --------------- preloader -------------------------------
+
+document.body.onload = function () {
+    setTimeout(function () {
+        let preloader = document.getElementById('page-preloader');
+        if( !preloader.classList.contains('preloader--done')) {
+            preloader.classList.add('preloader--done');
+        }
+    }, 2000);
+}
+
+window.onscroll = function() {
+  let navbar = document.getElementById("header");
+  let sticky = navbar.offsetTop + 1;
+
+      if (window.pageYOffset > sticky) {
+          navbar.classList.add("header--top")
+      } else {
+          navbar.classList.remove("header--top");
+      }
+}
+
