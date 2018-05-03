c6c838b45576f8f75b2a836e6bda3612694d20ba
osadnriets@gmail.com <gjx2890-12345>
2018-05-03 09:50:12 +0300

    add preloader

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@
 require('./main.scss');
 // --------------- preloader -------------------------------
 
-// document.body.onload = function () {
-//     setTimeout(function () {
-//         let preloader = document.getElementById('page-preloader');
-//         if( !preloader.classList.contains('preloader--done')) {
-//             preloader.classList.add('preloader--done');
-//         }
-//     }, 2000);
-// }
+ document.body.onload = function () {
+     setTimeout(function () {
+         let preloader = document.getElementById('page-preloader');
+         if( !preloader.classList.contains('preloader--done')) {
+             preloader.classList.add('preloader--done');
+         }
+     }, 2000);
+ }
 
 window.onscroll = function() {
   let navbar = document.getElementById("header");
